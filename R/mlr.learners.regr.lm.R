5aaf6c9a5b9a124380ec1b2475b1587b1d772bd4
Stefan Coors <stefan.coors@gmx.net>
2017-10-07 12:54:21 +0200

    parset of lm to phng

--- R/mlr.learners.regr.lm.R
+++ R/mlr.learners.regr.lm.R
@@ -1,9 +1,11 @@
 mlr.learners$add(LearnerRegr$new(
   name = "lm",
   package = "stats",
-  par.set = makeParamSet(
-    makeNumericLearnerParam(id = "tol", default = 1e-7, lower = 0),
-    makeLogicalLearnerParam(id = "singular.ok", default = TRUE, tunable = FALSE)
+  par.set = ParamSetFlat$new(
+    params = list(
+      ParamReal$new(id = "tol", default = 1e-7, lower = 0),
+      ParamFlag$new(id = "singular.ok", default = TRUE)
+    )
   ),
   par.vals = list(),
   properties = c("feat.numeric", "feat.factor", "se", "weights"),
--- R/mlr.learners.regr.rpart.R
+++ R/mlr.learners.regr.rpart.R
@@ -1,6 +1,7 @@
 mlr.learners$add(LearnerRegr$new(
   name = "rpart",
   package = "rpart",
+  par.set = ParamSetFlat$new(
     params = list(
       ParamInt$new(id = "minsplit", default = 20L, lower = 1L),
       ParamReal$new(id = "cp", default = 0.01, lower = 0, upper = 1),
@@ -11,6 +12,7 @@ mlr.learners$add(LearnerRegr$new(
       ParamInt$new(id = "maxdepth", default = 30L, lower = 1L, upper = 30L), # we use 30 as upper limit, see docs of rpart.control
       ParamInt$new(id = "xval", default = 10L, lower = 0L)
     )
+  ),
   par.vals = list(),
   properties = c("missings", "feat.numeric", "feat.factor", "feat.ordered", "oobpreds", "featimp", "se", "formula"), # FIXME: Does rpart really support se estimation?
   train = function(task, subset, ...) {
