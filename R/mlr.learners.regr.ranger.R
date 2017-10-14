e1932dfe0893fb0c143c6823c0f382ad42f53b76
Stefan Coors <stefan.coors@gmx.net>
2017-10-14 09:33:36 +0200

    rewrote all learner's param sets to phng

--- R/mlr.learners.regr.ranger.R
+++ R/mlr.learners.regr.ranger.R
@@ -1,41 +1,51 @@
 mlr.learners$add(LearnerRegr$new(
   name = "ranger",
   package = "ranger",
-  par.set = makeParamSet(
-    makeIntegerLearnerParam(id = "num.trees", lower = 1L, default = 500L),
-    # FIXME: Add default value when data dependent defaults are implemented: mtry=floor(sqrt(#independent vars))
-    makeIntegerLearnerParam(id = "mtry", lower = 1L),
-    makeIntegerLearnerParam(id = "min.node.size", lower = 1L, default = 5L),
-    makeLogicalLearnerParam(id = "replace", default = TRUE),
-    makeNumericLearnerParam(id = "sample.fraction", lower = 0L, upper = 1L),
-    makeNumericVectorLearnerParam(id = "split.select.weights", lower = 0, upper = 1),
-    makeUntypedLearnerParam(id = "always.split.variables"),
-    makeLogicalLearnerParam(id = "respect.unordered.factors", default = FALSE),
-    makeDiscreteLearnerParam(id = "importance", values = c("none", "impurity", "permutation"), default = "none", tunable = FALSE),
-    makeLogicalLearnerParam(id = "write.forest", default = TRUE, tunable = FALSE),
-    makeLogicalLearnerParam(id = "scale.permutation.importance", default = FALSE, requires = quote(importance == "permutation"), tunable = FALSE),
-    makeIntegerLearnerParam(id = "num.threads", lower = 1L, when = "both", tunable = FALSE),
-    makeLogicalLearnerParam(id = "save.memory", default = FALSE, tunable = FALSE),
-    makeLogicalLearnerParam(id = "verbose", default = TRUE, when = "both", tunable = FALSE),
-    makeIntegerLearnerParam(id = "seed", when = "both", tunable = FALSE),
-    makeDiscreteLearnerParam(id = "splitrule", values = c("variance", "maxstat"), default = "variance"),
-    makeNumericLearnerParam(id = "alpha", lower = 0L, upper = 1L, default = 0.5, requires = quote(splitrule == "maxstat")),
-    makeNumericLearnerParam(id = "minprop", lower = 0L, upper = 1L, default = 0.1, requires = quote(splitrule == "maxstat")),
-    makeLogicalLearnerParam(id = "keep.inbag", default = FALSE, tunable = FALSE)
+  par.set = ParamSetFlat$new(
+    params = list(
+      ParamInt$new(id = "num.trees", lower = 1L, default = 500L),
+      # FIXME: Add default value when data dependent defaults are implemented: mtry=floor(sqrt(#independent vars))
+      ParamInt$new(id = "mtry", lower = 1L),
+      ParamInt$new(id = "min.node.size", lower = 1L, default = 5L),
+      ParamFlag$new(id = "replace", default = TRUE),
+      ParamReal$new(id = "sample.fraction", lower = 0L, upper = 1L),
+      ParamUntyped$new(id = "split.select.weights", lower = 0, upper = 1),
+      # FIXME: split.select.weights was vector param, untyped correct?
+      ParamFactor$new(id = "splitrule", values = c("variance", "maxstat"), default = "variance"),
+      ParamReal$new(id = "alpha", lower = 0L, upper = 1L, default = 0.5, special.vals = NA),
+      ParamReal$new(id = "minprop", lower = 0L, upper = 1L, default = 0.1, special.vals = NA),
+      ParamUntyped$new(id = "always.split.variables"),
+      ParamFlag$new(id = "respect.unordered.factors", default = FALSE),
+      ParamFactor$new(id = "importance", values = c("none", "impurity", "permutation"), default = "none"),
+      ParamFlag$new(id = "write.forest", default = TRUE),
+      ParamFlag$new(id = "scale.permutation.importance", default = FALSE, special.vals = NA),
+      ParamInt$new(id = "num.threads", lower = 1L, when = "both"),
+      ParamFlag$new(id = "save.memory", default = FALSE),
+      ParamFlag$new(id = "verbose", default = TRUE, when = "both"),
+      ParamInt$new(id = "seed", when = "both"),
+      ParamFlag$new(id = "keep.inbag", default = FALSE)
+    ),
+    restriction = quote(
+      (is.na(scale.permutation.importance) | importance == "permutation") &
+      (is.na(alpha) | splitrule == "maxstat") &
+      (is.na(minprop) | splitrule == "maxstat")
+    ),
+    par.set = makeParamSet(
+      mrequires = quo= quote(splitrule == "maxstat")),
   ),
   par.vals = list(num.threads = 1L, verbose = FALSE, respect.unordered.factors = TRUE),
   properties = c("formula", "feat.numeric", "feat.factor", "feat.ordered", "oobpreds", "featimp", "parallel"),
-
+  
   train = function(task, subset, ...) {
     data = getTaskData(task, subset = subset, type = "train", props = self$properties)
     ranger::ranger(formula = task$formula, data = data, ...)
   },
-
+  
   predict = function(model, newdata, ...) {
     p = predict(object = model$rmodel, data = newdata, ...)
     unname(p$predictions)
   },
-
+  
   model.extractors = list(
     OOBPredictions = function(model, task, subset, ...) {
       model$predictions
